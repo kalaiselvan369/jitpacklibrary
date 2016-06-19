a20c0e2f65370ddb27b4c451b569b36ad02e1764
kalaiselvan <kalaiselvan.1@tcs.com>
2016-06-19 20:40:05 +0530

    first commit

---
+++ devcorelibrary/src/main/java/com/example/devcorelibrary/SampleModelCore.java
@@ -0,0 +1,11 @@
+package com.example.devcorelibrary;
+
+/**
+ * Created by Airbase on 19-Jun-16.
+ */
+public class SampleModelCore {
+
+    public static String getName() {
+        return "user your github library project as dependency";
+    }
+}
